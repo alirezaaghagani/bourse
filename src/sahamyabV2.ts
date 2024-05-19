11f95abf5ff1cf304c387141f014a0a6bae1d2c7
alirezaaghagani <alirezaaghagani@gmail.com>
2024-05-19 12:50:07 +0330

    shamyaba v2 second test

--- src/sahamyabV2.ts
+++ src/sahamyabV2.ts
@@ -9,11 +9,11 @@ const TOKEN = {
 };
 const tokenPasted = {
   access_token:
-    "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJkMEkzcHg5c2x2VkpCWDg1ODdTcjF5ZU1uMEUwZjRRb21lR08zMngyeVQ4In0.eyJqdGkiOiI0YzMyMzVlZS0zOGNjLTQzNWMtYWYyYS1kNWRmNzA1MmNlNjEiLCJleHAiOjE3MTYwNjMyMzQsIm5iZiI6MCwiaWF0IjoxNzE2MDYxNDM0LCJpc3MiOiJodHRwOi8va2V5Y2xvYWs6OTA4MC9hdXRoL3JlYWxtcy9zYWhhbXlhYiIsImF1ZCI6InNhaGFteWFiIiwic3ViIjoiYmZmMTBiOWUtY2U5Mi00NWU5LWJiM2QtYTczYzhmODYwMjYyIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic2FoYW15YWIiLCJhdXRoX3RpbWUiOjAsInNlc3Npb25fc3RhdGUiOiJlNzFjNTc5MS1hOTQ0LTQzYzMtOGI2Yy02NTMzMWRkYmU5ZGQiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vbmVvYnJva2VyLmlyIiwiaHR0cHM6Ly8qLnNhaGFteWFiLmNvbSIsImh0dHBzOi8vZGV2Lm5lb2Jyb2tlci5pciIsImh0dHBzOi8vd3d3Lm5lb2Jyb2tlci5pciJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiUk9MRV9VU0VSIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiZW1haWwgcHJvZmlsZSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicGhvbmVfbnVtYmVyX3ZlcmlmaWVkIjp0cnVlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJhbGlyZXphYWIwMDciLCJnaXZlbl9uYW1lIjoi2LnZhNuM2LHYttinIiwicHJvZmlsZV9pbWFnZSI6ImRlZmF1bHQiLCJpbmFjdGl2ZSI6ZmFsc2UsInVzZXJfaWQiOjQ1MDAwMDE2MSwibmFtZSI6Iti52YTbjNix2LbYpyIsIm5pY2tuYW1lIjoi2LnZhNuM2LHYttinIiwicGhvbmVfbnVtYmVyIjoiMDkyMTA3NTg2NTUiLCJjb3Zlcl9pbWFnZSI6ImRlZmF1bHQiLCJvbmxpbmVfdXNlciI6ZmFsc2UsIm9mZmljaWFsX3VzZXIiOmZhbHNlLCJlbWFpbCI6ImFsaXJlemFhZ2hhZ2FuaS5hYUBnbWFpbC5jb20iLCJwcml2YXRlX21zZ19pZCI6ImJlMDBlZTYyLTY5MmQtNGVlYy1iN2MwLTBhY2JkMzk2YjNkNSJ9.ZaFp4phCYzc3PuVl52cmojR7tTGjBSqgPPqwmpIuQxDXI5HMCk5VBF9N7zfag9YuUhUl2HNfSRLW9RkjU7Ccb0HweXIm8zbgAG5DSbsXrSgpghTULw9jZoEuR6babwjXn6fbxBjWcoOrwFYztHheoYP2U3qdIQNz9Wsd7Jvxl4p8UTM8igFSL67BdIUBGbNxU8iBw4_vR9Wwos-xPT7NUZp2ARPObuWFFurQMsCx66p-yTRhvpnmQEVBAg1jzIXSqHclZDuzazJMZXQK-oauLNgwXBYUMxQYPgw5Wh_43ZG-Ik7jllx58CTUYmBJWoeV6u1J2viN4dvAkW-259CKig",
+    "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJkMEkzcHg5c2x2VkpCWDg1ODdTcjF5ZU1uMEUwZjRRb21lR08zMngyeVQ4In0.eyJqdGkiOiIyOTlkOWUxNC1jOTE0LTQxOWEtYWUwNC0yYjZiOWFkZTg3N2EiLCJleHAiOjE3MTYxMTIwODYsIm5iZiI6MCwiaWF0IjoxNzE2MTEwMjg2LCJpc3MiOiJodHRwOi8va2V5Y2xvYWs6OTA4MC9hdXRoL3JlYWxtcy9zYWhhbXlhYiIsImF1ZCI6InNhaGFteWFiIiwic3ViIjoiYmZmMTBiOWUtY2U5Mi00NWU5LWJiM2QtYTczYzhmODYwMjYyIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic2FoYW15YWIiLCJhdXRoX3RpbWUiOjAsInNlc3Npb25fc3RhdGUiOiJlNzFjNTc5MS1hOTQ0LTQzYzMtOGI2Yy02NTMzMWRkYmU5ZGQiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vbmVvYnJva2VyLmlyIiwiaHR0cHM6Ly8qLnNhaGFteWFiLmNvbSIsImh0dHBzOi8vZGV2Lm5lb2Jyb2tlci5pciIsImh0dHBzOi8vd3d3Lm5lb2Jyb2tlci5pciJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiUk9MRV9VU0VSIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiZW1haWwgcHJvZmlsZSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicGhvbmVfbnVtYmVyX3ZlcmlmaWVkIjp0cnVlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJhbGlyZXphYWIwMDciLCJnaXZlbl9uYW1lIjoi2LnZhNuM2LHYttinIiwicHJvZmlsZV9pbWFnZSI6ImRlZmF1bHQiLCJpbmFjdGl2ZSI6ZmFsc2UsInVzZXJfaWQiOjQ1MDAwMDE2MSwibmFtZSI6Iti52YTbjNix2LbYpyIsIm5pY2tuYW1lIjoi2LnZhNuM2LHYttinIiwicGhvbmVfbnVtYmVyIjoiMDkyMTA3NTg2NTUiLCJjb3Zlcl9pbWFnZSI6ImRlZmF1bHQiLCJvbmxpbmVfdXNlciI6ZmFsc2UsIm9mZmljaWFsX3VzZXIiOmZhbHNlLCJlbWFpbCI6ImFsaXJlemFhZ2hhZ2FuaS5hYUBnbWFpbC5jb20iLCJwcml2YXRlX21zZ19pZCI6ImJlMDBlZTYyLTY5MmQtNGVlYy1iN2MwLTBhY2JkMzk2YjNkNSJ9.dQb1auUgCt3IFw0VckpzJZJjMNkYlwWpUSu6O3_MMyI3vOJI36ST5W9EdK_Va_w4C4csevO5-dgMPY-Y9wj9sf4U5TUcEKmn3YLcGfw248NoUtenElQ9dWAt52vrrz0Qy3DY9SWhaTPRZj0QG9fSaqHfHd-KHMaWqwKd_9yQRpNMsqrplp7GhGUZWzAgnyini2g6HXBKn13LvZLbPzRJK4_REX04NuWgpLFDzsX_h9IjuWghFsHel8heZyBuMYK2gdTxCjp0HhikHfSbgfePTX50038UWc3r2g2Buy9Tp4LmYaoH_XTfsxu--8iPPyYZopJCMmIBWHfFOqFIfj0Bzg",
   expires_in: 1800,
-  refresh_expires_in: 2513255,
+  refresh_expires_in: 2464403,
   refresh_token:
-    "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJkMEkzcHg5c2x2VkpCWDg1ODdTcjF5ZU1uMEUwZjRRb21lR08zMngyeVQ4In0.eyJqdGkiOiJiMDI0ZWY2ZC00NjNmLTRjOWEtOWE1Ny02NGQ3NjdmMWJkMDQiLCJleHAiOjE3MTg1NzQ2ODksIm5iZiI6MCwiaWF0IjoxNzE2MDYxNDM0LCJpc3MiOiJodHRwOi8va2V5Y2xvYWs6OTA4MC9hdXRoL3JlYWxtcy9zYWhhbXlhYiIsImF1ZCI6InNhaGFteWFiIiwic3ViIjoiYmZmMTBiOWUtY2U5Mi00NWU5LWJiM2QtYTczYzhmODYwMjYyIiwidHlwIjoiUmVmcmVzaCIsImF6cCI6InNhaGFteWFiIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiZTcxYzU3OTEtYTk0NC00M2MzLThiNmMtNjUzMzFkZGJlOWRkIiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIlJPTEVfVVNFUiIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUifQ.dT-qgaRJ9TD4PdlmBIHZv7Tg6Jx6kMxELarDcIyy__Yd4HrNPeVOHMaTSYRympxNrNgiBJVlw4B2iVjgt-YN1or9XhzibM0yMAB0hOc3VBSNzVWTgJKNBJkVwi8ocivDnfnmOQ_YZeNFpgJ2c_AS33pyXZ3UtS-UAb4kFpUb9r5SFfa9jba_6E3J59IKEwhOgDvHBG1UEKy3APXtXPgjHPzTbnksMzuEPs-pCvNciVuxwewhFuWlQUwdTgfVzuvTMbQibRvuAiMh7T3x6X6m_DUqhr3ayPAk_kIJs2b0K_7ujX_otJmryBMY969c_8-EhOeiO2SSaKHY4TW-JVC9Ww",
+    "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJkMEkzcHg5c2x2VkpCWDg1ODdTcjF5ZU1uMEUwZjRRb21lR08zMngyeVQ4In0.eyJqdGkiOiI5NGMwM2ViMC04ZjJlLTRjMmQtOTc2OC04ZDNjODY1ODBmZTUiLCJleHAiOjE3MTg1NzQ2ODksIm5iZiI6MCwiaWF0IjoxNzE2MTEwMjg2LCJpc3MiOiJodHRwOi8va2V5Y2xvYWs6OTA4MC9hdXRoL3JlYWxtcy9zYWhhbXlhYiIsImF1ZCI6InNhaGFteWFiIiwic3ViIjoiYmZmMTBiOWUtY2U5Mi00NWU5LWJiM2QtYTczYzhmODYwMjYyIiwidHlwIjoiUmVmcmVzaCIsImF6cCI6InNhaGFteWFiIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiZTcxYzU3OTEtYTk0NC00M2MzLThiNmMtNjUzMzFkZGJlOWRkIiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIlJPTEVfVVNFUiIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6ImVtYWlsIHByb2ZpbGUifQ.V1-cH-379nUxrckDMSpQbU4_lU7cr10OYtMdmes-E66faoGebzd9I9GRXTNk5ivowdBrnWqRLiDDuJ0szl0v_P6nTjGUkaSfYahWMmvD6slJHXZaY1DpK3Jkv3zTVHxysixfMU6OybzobdCpHCnFgvmApRkIT1Yi3ZrWWnRnOkxX3ARZ5SVxH2n0mn3nUYw_W9_gEbDITYYMMpn56Z7fgdB3K8i5GYwuj-Q2W5ux0FBIHwDy1BQ7yy-f79i8kp0WCNxZLUt1al89mWnbcUCM5pjVDVinihZBrtfdj6oJUo5hR6fhpiAUcCV8liTozO59zHjHqQ8lQr7qWm3Hgsv1xg",
   token_type: "bearer",
   "not-before-policy": 1538380001,
   session_state: "e71c5791-a944-43c3-8b6c-65331ddbe9dd",
@@ -32,7 +32,7 @@ export async function scrapeSahamyabV2() {
   // * make scrape continue from last section
   let page = 0;
   let lastId = await getLastItemIdFromDb<number>(db);
-  let id = lastId || 450462813;
+  let id = lastId || 144024851;
 
   console.log("+-----Id:", id);
   // let itemsList: twitItemObj[] = [];
@@ -41,7 +41,14 @@ export async function scrapeSahamyabV2() {
   for (let i = 0; i < REQ_NUM; i++) {
     try {
       let data = await scrapeSahamyabById(page, id, i);
-      id = data?.lastId!;
+      const lastId = data?.lastId!;
+
+      if (id - lastId < 250) id = lastId;
+      else {
+        console.log("long seperetion in data detected:", id - lastId);
+        id--;
+        continue;
+      }
       if (data?.items.length) {
         const filteredItems = data?.items.filter(
           (item) => item.advertise !== true
@@ -53,7 +60,7 @@ export async function scrapeSahamyabV2() {
         if (!data.hasMore) break;
 
         // * refresh page(because of 10 pagination limit)
-        if (page < 10) page++;
+        if (page < 20) page++;
         else page = 0;
       }
       // * refresh token every <REFRESH_TOKEN_INTERVAL> time (because of 1800 time limit)
@@ -63,7 +70,7 @@ export async function scrapeSahamyabV2() {
         TOKEN.refresh_token = newToken.refresh_token;
         console.log("token refreshed to:", newToken);
       }
-      await sleep(250);
+      await sleep(300);
     } catch (error) {
       if (error instanceof Error) {
         console.log("Error: ", error.name, "->", error.message);
@@ -84,10 +91,10 @@ async function scrapeSahamyabById(page: Number, id: number, index: Number) {
     );
     const data: twitsResObj = res.data;
     if (!data.success) throw new Error(`page:${page} id:${id} not Found!`);
-    const lastId = data.items.at(-1)?.id;
+    const lastId = data.items.at(-1)?.id || id - 1;
     const countLeft = id - FINISH_ID;
     console.log(
-      `${index} - ${res.status} -----page: ${page} id: ${id} - ${lastId} (${countLeft} left!).`
+      `${index} - ${res.status} -----page: ${page} id: ${id} - ${lastId} >> (${countLeft} left!).`
     );
 
     return { lastId: Number(lastId), items: data.items, hasMore: data.hasMore };
